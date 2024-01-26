c314755f5d71847cc3559ce5c4c7a555d28c1491
Ngo Viet Hoang <ngoviethoang007@gmail.com>
2024-01-26 11:15:11 +0700

    update light & dark mode p1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import { Button, Typography } from '@mui/material'
 import DeleteIcon from '@mui/icons-material/Delete'
+import {
+  useColorScheme
+} from '@mui/material/styles'
 
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 
 function App() {
 
   return (
     <>
+      <ModeToggle/>
       <div>hoang ngo</div>
       <Button variant="text">Text</Button>
       <Button variant="contained" color ="success">Contained</Button>
