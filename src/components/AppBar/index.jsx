f2997c19b61c5e653f94a276ddc029773b9280fb
Ngo Viet Hoang <ngoviethoang007@gmail.com>
2024-02-04 20:33:18 +0700

    update app bar p3

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -28,6 +28,8 @@ function AppBar() {
         display: "flex",
         alignItems: "center",
         justifyContent: "space-between",
+        gap: 2,
+        overflowX: "auto",
       }}
     >
       <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
@@ -45,11 +47,13 @@ function AppBar() {
             Trello
           </Typography>
         </Box>
-        <Workspaces />
-        <Recent />
-        <Starred />
-        <Templates />
-        <Button variant="outlined">Create</Button>
+        <Box sx={{ display: { xs: "none", md: "flex" }, gap: 1 }}>
+          <Workspaces />
+          <Recent />
+          <Starred />
+          <Templates />
+          <Button variant="outlined">Create</Button>
+        </Box>
       </Box>
       <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
         <TextField
@@ -57,6 +61,7 @@ function AppBar() {
           id="outlined-search"
           label="Search..."
           type="search"
+          sx={{ minWidth: "120px" }}
         />
         <ModeSelect />
         <Tooltip title="Notification">
